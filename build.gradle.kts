9f5746a2e2469ab872b9838fc73bc17da9042a4a
Kirill Himura <frogharvard@gmail.com>
2024-02-04 22:49:22 +0800

    chore(dev): set the dev mode on
    - Also did some init changes

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,23 +1,20 @@
-val ktor_version: String by project
-val kotlin_version: String by project
-val logback_version: String by project
+val ktorVersion: String by project
+val kotlinVersion: String by project
+val logbackVersion: String by project
+val postgresVersion: String by project
+val h2Version: String by project
 
-val postgres_version: String by project
-val h2_version: String by project
 plugins {
   kotlin("jvm") version "1.9.22"
   id("io.ktor.plugin") version "2.3.7"
   id("org.jetbrains.kotlin.plugin.serialization") version "1.9.22"
 }
 
-group = "com.kenkoro"
+group = "com.kenkoro.taurus.api.client"
 version = "0.0.1"
 
 application {
   mainClass.set("io.ktor.server.netty.EngineMain")
-
-  val isDevelopment: Boolean = project.ext.has("development")
-  applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
 }
 
 repositories {
@@ -31,10 +28,11 @@ dependencies {
   implementation("io.ktor:ktor-server-call-logging-jvm")
   implementation("io.ktor:ktor-server-content-negotiation-jvm")
   implementation("io.ktor:ktor-serialization-kotlinx-json-jvm")
-  implementation("org.postgresql:postgresql:$postgres_version")
-  implementation("com.h2database:h2:$h2_version")
+
+  implementation("org.postgresql:postgresql:$postgresVersion")
   implementation("io.ktor:ktor-server-netty-jvm")
-  implementation("ch.qos.logback:logback-classic:$logback_version")
+  implementation("ch.qos.logback:logback-classic:$logbackVersion")
+
   testImplementation("io.ktor:ktor-server-tests-jvm")
-  testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+  testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlinVersion")
 }
--- src/main/kotlin/com/kenkoro/taurus/api/client/plugins/Routing.kt
+++ src/main/kotlin/com/kenkoro/taurus/api/client/plugins/Routing.kt
@@ -1,10 +1,14 @@
 package com.kenkoro.taurus.api.client.plugins
 
+import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 fun Application.configureRouting() {
   routing {
+    get("/") {
+      call.respondText(contentType = ContentType.Any, status = HttpStatusCode.OK) { "Hello!" }
+    }
   }
 }
