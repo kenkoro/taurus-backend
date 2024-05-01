c641cc2293385e6352d95878f36f6753bd6dc3c4
Kirill Himura <frogharvard@gmail.com>
2024-05-01 12:04:41 +0700

    feat: user_profile enum

--- init_dependencies.sh
+++ init_dependencies.sh
@@ -1,7 +1,17 @@
 export PGPASSWORD=postgres
 psql -U postgres> -d taurus << EOF
   begin;
-  create type order_status as enum ('Idle', 'Cut', 'Checked');
+    create type order_status as enum ('Idle', 'Cut', 'Checked');
+    create type user_profile as enum (
+      'Admin',
+      'Ceo',
+      'Customer',
+      'Manager',
+      'Cutter',
+      'Inspector',
+      'Tailor',
+      'Other'
+    );
   commit;
 EOF
 
