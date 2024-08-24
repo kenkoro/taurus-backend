acbf88600a98f0acf57d196c4a15e7aaae984f84
Ken Koro <kenjirokoro.business@gmail.com>
2024-08-24 06:00:57 -0400

    chore: server port fixes

--- utils/exports.sh
+++ utils/exports.sh
@@ -1,9 +1,9 @@
 #!/bin/zsh
 
-export JWT_SECRET="secret"
+export JWT_SECRET="!+&Aq6DaA}Y$0Mp"
 export PSQL_URL="jdbc:postgresql://localhost:5432/taurus"
-export PSQL_USER="kenkoro"
-export PSQL_PASSWORD="kenkoro"
+export PSQL_USER="www"
+export PSQL_PASSWORD="x9E{j^;5e8[Y"
 export KEY_STORE_PASSWORD="669631478910097"
 export KEY_STORE_ALIAS="self-signed-cert"
-export PGPASSWORD="kenkoro"
+export PGPASSWORD="SUk?j1f*48w,"
